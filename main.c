409f5f691419ac591ba88bc31ae212aaec873a4e
Mukilan K S <mukilnikil7@gmail.com>
2026-02-19 00:37:27 +0530

    Added more bytecode and other improvements

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include <stdint.h>
 #include <string.h>
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
 
 //struct definition for registers
 typedef union {
-	uint16_t x;
+	u16 x;
 	struct 
 	{
   		#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        		uint8_t l;
-        		uint8_t h;
+        		u8 l;
+        		u8 h;
 		#else
-        		uint8_t h;
-        		uint8_t l;
+        		u8 h;
+        		u8 l;
 		#endif	
 	};
 }reg16;
@@ -25,34 +29,34 @@ typedef struct{
 	reg16 ax,bx,cx,dx;
 	reg16 sp,bp,si,di;
 
-	uint16_t ip;
+	u16 ip;
 
-	uint16_t cs,ds,es,ss;
+	u16 cs,ds,es,ss;
 
-	uint16_t flags;
+	u16 flags;
 } CPU8086;
 
 #define MEM_SIZE (1024*1024)
-uint8_t memory[1024*1024];
-
-
+u8 memory[1024*1024];
 
 /**
  * This function only calculates the corresponding physical address
  * for the 8086. 32 bit is used because the address will always be
  * 20 bit and C does not have a native 20 bit type.
  **/
-uint32_t 
-phy(uint16_t segment ,uint16_t offset) 
+u32 phy(u16 segment, u16 offset) 
 {
-	return ((uint32_t)segment << 4 ) + offset;
+
+	return ((u32)segment << 4) + offset;
 }
 
+
+
 //Instruction pointer fetch
-uint8_t
+u8
 fetch8(CPU8086 *cpu)
 {
-	uint32_t addr = phy(cpu->cs,cpu->ip);
+	u32 addr = phy(cpu->cs,cpu->ip);
 	if (addr >= MEM_SIZE){
 		printf("Segmentation fault at %04X:%04X\n",cpu->cs,cpu->ip);
 		exit(1);
@@ -61,20 +65,19 @@ fetch8(CPU8086 *cpu)
 	return memory[addr];
 }
 
-uint16_t
+u16
 fetch16(CPU8086 *cpu)
 {
-	uint8_t lo =fetch8(cpu);
-	uint8_t hi =fetch8(cpu);
+	u8 lo =fetch8(cpu);
+	u8 hi =fetch8(cpu);
 	return (hi << 8) | lo;
 }
 
-static
-reg16* reg_table(CPU8086 *cpu, int index)
-{
+static reg16* reg_table(CPU8086 *cpu, int index) 
+{ 
 	switch(index)
-	{
-		case 0: return &cpu->ax;
+	{ 
+		case 0: return &cpu->ax; 
 		case 1: return &cpu->cx;
 		case 2: return &cpu->dx;
 		case 3: return &cpu->bx;
@@ -82,11 +85,11 @@ reg16* reg_table(CPU8086 *cpu, int index)
 		case 5: return &cpu->bp;
 		case 6: return &cpu->si;
 		case 7: return &cpu->di;
-		default:
-			fprintf(stderr, "Invalid register index %d\n", index);
-			exit(1);
+		default: fprintf(stderr, "Invalid register index %d\n", index); exit(1); 
 	}
 }
+
+
 static
 const char *reg_names[8] = {
     "AX","CX","DX","BX","SP","BP","SI","DI"
@@ -95,7 +98,7 @@ const char *reg_names[8] = {
 
 //load assembly language into memory 
 void 
-load_binary(const char *filename,uint16_t segment, uint16_t offset)
+load_binary(const char *filename,u16 segment, u16 offset)
 {
 	FILE *file = fopen(filename,"rb");
 	if (!file)
@@ -104,7 +107,7 @@ load_binary(const char *filename,uint16_t segment, uint16_t offset)
 		exit(1);
 	}
 
-	uint32_t addr = phy(segment,offset);
+	u32 addr = phy(segment,offset);
 
 	size_t n;
 	
@@ -137,26 +140,40 @@ reset_cpu(CPU8086 *cpu)
 	memset(cpu,0,sizeof(*cpu));
 }
 
-
-
 int
 execute_instruction(CPU8086 *cpu)
 {
-	uint8_t inst = fetch8(cpu);
+	u8 inst = fetch8(cpu);
 	printf("CS:IP %04X:%04X  Opcode: %02X\n",
            cpu->cs, cpu->ip - 1, inst);
 	switch(inst)
 	{
 		case 0xB8 ... 0xBF:
 		{
-			uint16_t imm = fetch16(cpu);
-			
+			u16 imm = fetch16(cpu);
 			int index = inst - 0xB8;
-
 			reg_table(cpu,index)->x = imm;
 			printf("MOV %s , %04X\n",reg_names[index],imm);
 			break;
 		}			
+		
+		case 0xB0 ... 0xB7:
+		{
+    			u8 imm = fetch8(cpu);
+    			switch(inst){
+        			case 0xB0: cpu->ax.l = imm; break; // AL
+        			case 0xB1: cpu->cx.l = imm; break; // CL
+        			case 0xB2: cpu->dx.l = imm; break; // DL
+        			case 0xB3: cpu->bx.l = imm; break; // BL
+        			case 0xB4: cpu->ax.h = imm; break; // AH
+        			case 0xB5: cpu->cx.h = imm; break; // CH
+        			case 0xB6: cpu->dx.h = imm; break; // DH
+				case 0xB7: cpu->bx.h = imm; break; // BH
+    			}
+			break;
+		}
+		
+			
 		case 0xF4:
 			printf("HLT\n");
 			dump_memory(cpu);
@@ -176,4 +193,3 @@ main()
 	cpu.ip=0x0000;
 	while(execute_instruction(&cpu));
 }
-
